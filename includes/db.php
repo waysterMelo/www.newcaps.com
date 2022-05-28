47395c5dbd482f9fdf220ba9ecf915e74c5a3d0b
Wayster cruz de melo <57998761+waysterMelo@users.noreply.github.com>
2022-05-27 21:46:09 -0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "ecom_db");
+$con = mysqli_connect("eyvqcfxf5reja3nv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "nokmeumjpe3winiz", "hblpn16b9omktn4c", "hzo2cj8j7gmp7gjs");
 
 ?>
